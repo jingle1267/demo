a80378c9d9943a24e3a2005d0d9c148ed9a523c7
jinzhenguo <jinzhenguo@jd.com>
2020-01-21 15:18:28 +0800

    [UPDATE] Update image path.

--- src/imagetranstion/imagetranstion.js
+++ src/imagetranstion/imagetranstion.js
@@ -18,7 +18,7 @@ function init() {
   var slide = new Slide(width, height, 'out');
 	var l1 = new THREE.ImageLoader();
 	l1.setCrossOrigin('Anonymous');
-	l1.load('./images/winter.jpg', function(img) {
+	l1.load('../images/winter.jpg', function(img) {
 	  slide.setImage(img);
 	})
   root.scene.add(slide);
@@ -26,7 +26,7 @@ function init() {
   var slide2 = new Slide(width, height, 'in');
   var l2 = new THREE.ImageLoader();
 	l2.setCrossOrigin('Anonymous');
-	l2.load('./images/spring.jpg', function(img) {
+	l2.load('../images/spring.jpg', function(img) {
 		slide2.setImage(img);
 	})
 	
